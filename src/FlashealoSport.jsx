6401e0912695b400da21a4f97c0f25a8cd1178d5
MarlonraM <marlon.ramirez.e@gmail.com>
2026-07-31 21:17:39 -0400

    Version Pixiest con 3 secciones SPORT REVISADA COn CAMARA BIOMETRICA oracle REV6

--- src/FlashealoSport.jsx
+++ src/FlashealoSport.jsx
@@ -71,7 +71,7 @@ const manejarFotosCapturadas = async (fotosBiometricas) => {
       setModalCamaraAbierto(false); 
       // Opcional: Podrías poner un estado de "setCargandoVector(true)" para mostrar un loader
       
-      const URL_API = "https://tu-enlace-de-cloudflare.trycloudflare.com/vectorizar-selfie/";
+      const URL_API = "https://velvet-nitrogen-surf-suggests.trycloudflare.com";
       
       const response = await fetch(URL_API, {
         method: "POST",
